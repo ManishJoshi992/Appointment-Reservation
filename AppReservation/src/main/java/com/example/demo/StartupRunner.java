5fa9ab2a04a83c8df9d84bf4af6c7f3504fbcb6a
Tirth Raj Joshi <joshimanish992@gmail.com>
2021-08-26 12:49:19 -0400

    comment out the test codes

--- AppReservation/src/main/java/com/example/demo/StartupRunner.java
+++ AppReservation/src/main/java/com/example/demo/StartupRunner.java
@@ -35,7 +35,7 @@ public class StartupRunner implements CommandLineRunner {
 	@Transactional
 	public void run(String... args) throws Exception {
 
-		System.out.println("hi manish"); 
+		//System.out.println("hi manish"); 
 		
 		Person per = new Person();
 		per.setFirstName("user");
